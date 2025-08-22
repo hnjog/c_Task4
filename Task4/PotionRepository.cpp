92cc040c05cb3be2af9a6826dab0c349b0fcfb15
Hyun <hnjogo@gmail.com>
2025-08-22 17:02:24 +0900

    포션 저장소 클래스 생성

---
+++ Task4/PotionRepository.cpp
@@ -0,0 +1,106 @@
+﻿#include "PotionRepository.h"
+#include "PotionDictionary.h"
+#include<iostream>
+
+PotionRepository::PotionRepository(shared_ptr<PotionDictionary> potionDictionary)
+{
+	wPtrPotionDictionary = potionDictionary;
+}
+
+PotionRepository::~PotionRepository()
+{
+}
+
+void PotionRepository::InitializeStock(const string& potionName)
+{
+	if (potionStackMap.find(potionName) != potionStackMap.end())
+	{
+		cout << "이미 초기화된 포션입니다!" << '\n';
+		return;
+	}
+
+	if (auto potionLock = wPtrPotionDictionary.lock())
+	{
+		PotionRecipe recipe = potionLock->SearchRecipeByName(potionName);
+		if (recipe.GetPotionName() == "")
+		{
+			cout << "레시피가 존재하지 않는 포션 이름입니다!" << '\n';
+			return;
+		}
+	}
+
+	potionStackMap[potionName] = PotionStack();
+	cout << "포션 생성 완료!" << '\n';
+	cout << potionName << "을 " << MAX_STOCK << "개 생성하였습니다!" << '\n';
+}
+
+bool PotionRepository::DispensePotion(const string& potionName)
+{
+	if (PotionNameCheck(potionName) == false)
+		return false;
+
+	if (potionStackMap[potionName].remainCount >= MAX_STOCK)
+	{
+		cout << "포션 저장소에 이미 최대치인 "<< MAX_STOCK <<"만큼 존재합니다!" << '\n';
+		return false;
+	}
+
+	if (potionStackMap[potionName].emptyCount <= 0)
+	{
+		cout << "포션 저장소에 해당 포션의 빈 병이 없습니다!" << '\n';
+		cout << "먼저 저장소에 해당 포션의 빈 병을 반환해주세요!" << '\n';
+		return false;
+	}
+
+	potionStackMap[potionName].remainCount++;
+	potionStackMap[potionName].emptyCount--;
+	cout << "포션 생성 완료!" << '\n';
+	cout << potionName << "을 " << 1 << "개 만들었어요!" << '\n';
+	return true;
+}
+
+void PotionRepository::ReturnPotion(const string& potionName)
+{
+	if (PotionNameCheck(potionName) == false)
+		return;
+
+	if (potionStackMap[potionName].emptyCount >= MAX_STOCK)
+	{
+		cout << "빈 병이 이미 넘칩니다!" << '\n';
+		cout << "새로운 포션을 생성해주세요!" << '\n';
+		return;
+	}
+
+	potionStackMap[potionName].emptyCount++;
+	cout << "빈병 수거 완료!" << '\n';
+	cout << potionName << "의 빈병 개수 : " << potionStackMap[potionName].emptyCount <<'\n';
+}
+
+int PotionRepository::GetPotionStack(const string& potionName)
+{
+	if (PotionNameCheck(potionName) == false)
+		return 0;
+
+	return potionStackMap[potionName].remainCount;
+}
+
+bool PotionRepository::PotionNameCheck(const string& potionName) const
+{
+	if (potionStackMap.find(potionName) == potionStackMap.end())
+	{
+		cout << "저장소에 이런 포션은 없습니다!" << '\n';
+		return false;
+	}
+
+	if (auto potionLock = wPtrPotionDictionary.lock())
+	{
+		PotionRecipe recipe = potionLock->SearchRecipeByName(potionName);
+		if (recipe.GetPotionName() == "")
+		{
+			cout << "레시피가 존재하지 않는 포션 이름입니다!" << '\n';
+			return false;
+		}
+	}
+
+	return true;
+}
---
+++ Task4/PotionRepository.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include <unordered_map>
+#include <string>
+#include <memory>
+
+using namespace std;
+
+const int MAX_STOCK = 3;
+
+class PotionDictionary;
+
+struct PotionStack
+{
+public:
+	PotionStack()
+		:remainCount(MAX_STOCK),
+		emptyCount(0)
+	{
+
+	}
+
+	PotionStack(int remainCount, int emptyCount)
+		:remainCount(remainCount),
+		emptyCount(emptyCount)
+	{
+	}
+
+public:
+	int remainCount = 0;
+	int emptyCount = 0;
+};
+
+// 포션 제작 및 소모 관련 클래스
+class PotionRepository
+{
+public:
+	PotionRepository(shared_ptr<PotionDictionary> potionDictionary);
+	virtual ~PotionRepository();
+
+public:
+	// 포션을 MAX_STOCK 까지 초기화
+	void InitializeStock(const string& potionName);
+
+	// 포션 제작 시도, 성공 여부 return
+	bool DispensePotion(const string& potionName);
+
+	// 해당 이름의 포션 공병이 회수됨
+	void ReturnPotion(const string& potionName); 
+
+	// 포션의 remain / empty Count
+	int GetPotionStack(const string& potionName);
+
+private:
+	bool PotionNameCheck(const string& potionName) const;
+
+protected:
+	unordered_map<string, PotionStack> potionStackMap; // 현재 가진 포션 (공병 x)
+	weak_ptr<PotionDictionary> wPtrPotionDictionary;
+};
+
