3de929010846d012c9819f0c8b68eb4894ff2317
Hyun <hnjogo@gmail.com>
2025-08-22 12:43:12 +0900

    포션 레시피의 노출된 변수를 protected로 변환 후 getter 생성

--- Task4/PotionRecipe.h
+++ Task4/PotionRecipe.h
@@ -9,9 +9,14 @@ using namespace std;
 class PotionRecipe 
 {
 public:
-    string potionName;
-    vector<string> ingredients; // 단일 재료에서 재료 '목록'으로 변경
-
     // 생성자: 재료 목록을 받아 초기화하도록 수정
     PotionRecipe(const string& name, const vector<string>& ingredients);
+
+public:
+    const string& GetPotionName() const { return potionName; }
+    const vector<string>& GetIngredients() const { return ingredients; }
+
+protected:
+    string potionName;
+    vector<string> ingredients; // 단일 재료에서 재료 '목록'으로 변경
 };
